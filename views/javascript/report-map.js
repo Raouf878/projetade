7594c9689786e0fc6f9647a5b5c07bb4217a67d4
raouf <cheribetraouf607@gmail.com>
2023-06-21 00:27:29 +0100

    commit for changes done only login, register

--- views/javascript/report-map.js
+++ views/javascript/report-map.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     var ReportStatus=document.getElementById("report-info")
     var StatusButtons=document.getElementsByClassName("button")
-    var inputtt=document.getElementById('team-input')
     var ReportImg=document.getElementById("close-img")
+    let Gen_BTN =document.getElementById('gen-btn2')
+    let teamName = document.getElementById('team-input');
+    let incidentAddress = document.getElementById('address')
+    let incidentTitle = document.getElementById('title')
+    let incidentAffected = document.getElementById('affected')
+    let estimated_time=document.getElementById('desc')
+    let incidentLan = document.getElementById('Lan')
+    let incidentLat = document.getElementById('Lat')
+    var selectElement = document.getElementById("mySelect");
+    
     
     for(var i = 0; i < StatusButtons.length; i++) {
         
@@ -18,5 +27,13 @@ document.addEventListener("DOMContentLoaded", function() {
         ReportStatus.style.height = "0%";
         ReportStatus.style.opacity = 0;
         ReportStatus.style.bottom= "0%";
-    }
+        Gen_BTN.disabled=false;
+        teamName.value='';
+        incidentAddress.value=''
+        incidentAffected.value=''
+        incidentLan.value=''
+        incidentLat.value=''
+        incidentTitle.value=''
+        estimated_time.value=''
+                                }
 });
